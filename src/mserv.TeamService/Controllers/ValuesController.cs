3eb429f0a88288e75852d5973ae0f859cd2ba0b1
xiillii <jarommat@gmail.com>
2017-12-15 01:28:26 -0600

    first commit teamservice

--- src/mserv.TeamService/Controllers/ValuesController.cs
+++ src/mserv.TeamService/Controllers/ValuesController.cs
@@ -13,7 +13,7 @@ namespace mserv.TeamService.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] {"value1", "value2"};
+            return new string[] { "value1", "value2" };
         }
 
         // GET api/values/5
@@ -25,13 +25,13 @@ namespace mserv.TeamService.Controllers
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody] string value)
+        public void Post([FromBody]string value)
         {
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public void Put(int id, [FromBody]string value)
         {
         }
 
